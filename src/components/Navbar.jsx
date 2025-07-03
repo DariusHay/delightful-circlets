c68a2bd5d5ee890ff74cf049335c4c8c7146c7fe
Darius Hay <dariushay@Dariuss-MBP.lan>
2025-07-02 23:29:39 -0400

    changed links for header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Delightful Circlets</h1>
+      <h1><Link to="/">Delightful Circlets</Link></h1>
       <div>
         <Link to="/">Home</Link>
         <Link to="/shop">Shop</Link>
